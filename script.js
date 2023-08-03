cab520991cce035925fc8c8b527472fede5532b5
David Kim <dkhw90@gmail.com>
2023-08-03 17:29:56 +0200

    Add social media icons & fix minor bug with responsive css

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 
-const nav = document.querySelector('.nav');
+const navbar = document.querySelector('.navbar');
 const navBtn = document.querySelector('.nav-btn');
 const closeNav = document.querySelector('.close-icon');
 const sidebar = document.querySelector("#sidebar");
+const body = document.querySelector('body');
 
 navBtn.addEventListener('click', () => {
   sidebar.classList.add("show-sidebar");
+  body.classList.add("modal-open");
 });
 
 closeNav.addEventListener('click', () => {
   sidebar.classList.remove("show-sidebar");
+  body.classList.remove("modal-open");
 })
 
 // add fixed class to navbar
 // window.addEventListener("scroll", function () {
-//   if (window.scrollY > 80) {
-//     navbar.classList.add("navbar-fixed");
+//   if (window.scrollY > 30) {
+//     navbar.classList.add("sticky");
 //   } else {
-//     navbar.classList.remove("navbar-fixed");
+//     navbar.classList.remove("sticky");
 //   }
 // });
